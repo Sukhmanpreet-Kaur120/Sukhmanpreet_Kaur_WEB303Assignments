67164d06727ad53ea63ebc12ffbf04123809a677
Sukhmanpreet-Kaur120 <122814140+Sukhmanpreet-Kaur120@users.noreply.github.com>
2023-09-24 14:08:21 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,34 +1,40 @@
+//WEB 303 Assignment 2
 $(document).ready(function () {
   var content = $("#content");
-
-  function load(url) {
+  function load(url) 
+  {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
-
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          content.slideDown(4000, function () {
+    xhr.onreadystatechange = function () 
+    {
+      if (xhr.readyState === 4) 
+      {
+        if (xhr.status === 200) 
+        {
+          content.slideDown(4000, function () 
+          {
             content.html(xhr.responseText);
             content.slideUp(4000);
           });
-        } else {
+        } 
+        else 
+        {
           console.log("Error loading content.");
         }
       }
     };
-
     xhr.send();
   }
-  $("#prospect").click(function () {
+  $("#prospect").click(function () 
+  {
     load("prospect.html");
   });
-
-  $("#convert").click(function () {
+  $("#convert").click(function () 
+  {
     load("convert.html");
   });
-
-  $("#retain").click(function () {
+  $("#retain").click(function () 
+  {
     load("retain.html");
   });
 });
