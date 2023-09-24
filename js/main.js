2f64651c5cd245ff552f51680b760ba4b39af39f
Sukhmanpreet-Kaur120 <122814140+Sukhmanpreet-Kaur120@users.noreply.github.com>
2023-09-24 14:02:18 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,33 +1,36 @@
-$(document).ready(function (){
-    var content = $('content');
-    function loadContent(url) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", url, true);
-    
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-              content.slideDown(400, function () {
-                content.html(xhr.responseText);
-                content.slideUp(400);
-              });
-            } else {
-              console.log("Error loading content.");
-            }
-          };
-    
-        xhr.send();
+$(document).ready(function () {
+  var content = $("#content");
+
+  function loadContent(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          content.slideDown(400, function () {
+            content.html(xhr.responseText);
+            content.slideUp(400);
+          });
+        } else {
+          console.log("Error loading content.");
+        }
       }
-      $("#prospect").click(function () {
-        loadContent("prospect.html");
-      });
-    
-      $("#convert").click(function () {
-        loadContent("convert.html");
-      });
-    
-      $("#retain").click(function () {
-        loadContent("retain.html");
-      });
-    });
+    };
 
+    xhr.send();
+  }
 
+  // Attach click event handlers to the links
+  $("#prospect").click(function () {
+    loadContent("prospect.html");
+  });
+
+  $("#convert").click(function () {
+    loadContent("convert.html");
+  });
+
+  $("#retain").click(function () {
+    loadContent("retain.html");
+  });
+});
