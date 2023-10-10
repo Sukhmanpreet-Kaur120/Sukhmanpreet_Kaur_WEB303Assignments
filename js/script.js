38402108aa51f81c6e705b4b87f5f659a53d2323
Sukhmanpreet-Kaur120 <122814140+Sukhmanpreet-Kaur120@users.noreply.github.com>
2023-10-10 17:18:51 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,35 +14,24 @@ $(function () {
             let tude = pos.coords.latitude.toFixed(2);
             let long = pos.coords.longitude.toFixed(2);
             let acc = pos.coords.accuracy.toFixed(2);
-    
-        $("#locationhere").html("<p>Latitude: " +tude +"</p>" +
-        "<p>Longitude: " +long +"</p>" +
-        "<p>Accuracy: " +acc/1000 +" killometers</p>");
-
-        localStorage.setItem("loc",JSON.stringify({ latitude: tude, longitude: long }));
-        let storeLoc = localStorage.getItem("loc");
-            if (storeLoc) 
-            {
+            $("#locationhere").html("<p>Latitude: " +tude +"</p>" +"<p>Longitude: " +long +"</p>" +
+            "<p>Accuracy: " +acc/1000 +" killometers</p>");
+            localStorage.setItem("loc",JSON.stringify({ latitude: tude, longitude: long }));
+            let storeLoc = localStorage.getItem("loc");
+            if (storeLoc) {
                 storeLoc = JSON.parse(storeLoc);
-                
                 let storeLatitude = storeLoc.latitude;
                 let storeLongitude = storeLoc.longitude;
                 let dist = calcDistanceBetweenPoints(tude, long, storeLatitude, storeLongitude);
-
                 $("#locationhere").append("<p>Stored location: Latitude " +storeLatitude +" Longitude " +storeLongitude +"</p>"+
                 "<p> Distance traveled: " +(dist/1000).toFixed(2) +" killometers</p>");
-
-        $("header").append("<h1>Welcome Back!</h1>");
-        $("header").append("<h1> You have traveled " +(dist / 1000).toFixed(2) +" killometers.</h1>");
-
-            }
-            else 
-            {
-                $("header").append("<h1>Welcome to E Corp!</h1>");
-            }
+                $("header").append("<h1>Welcome Back!</h1>");
+                $("header").append("<h1> You have traveled " +(dist / 1000).toFixed(2) +" killometers.</h1>");
         }
-            function fail() 
-        {
+            else {
+                $("header").append("<h1>Welcome to E Corp!</h1>");
+            }}
+            function fail() {
             $("#locationhere").html("<h1>Please allow your geolocation</h1>");
         }
     }
