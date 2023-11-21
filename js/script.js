ee976b7d63e132dd32235ac0af534bca12cb8616
Sukhmanpreet-Kaur120 <122814140+Sukhmanpreet-Kaur120@users.noreply.github.com>
2023-11-21 08:18:53 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,60 +1,114 @@
-let charactersData;
-$(document).ready(function() {
-    $.ajax({
-        url: 'characters.json',
-        dataType: 'json',
-            success: function(data) {
-            charactersData = data; 
-            displayCharacters(data);
-            $('#search').on('input', function() {
-                highlightRows($(this).val());
-            });
-            $('#filterAM').on('click', function() {
-                filterCharacters('A', 'M');
-            });
-            $('#filterNZ').on('click', function() {
-                filterCharacters('N', 'Z');
-            });
-                },
-            error: function() {
-                alert('Failed to retrieve data.');
+// add a table to the page 
+let $table = $('<table/>');
+// add to the body of document
+$('body').append($table);
+
+// add a title for the table before table 
+$('table').before('<h1/>');
+$('h1').text('Sorting Characters');
+
+// add a thead and tbody on the table 
+$('table').append('<thead/>');
+$('table').append('<tbody/>');
+$('table').addClass('sortable');
+
+//create the heading row
+let $headingRow = $('<tr/>');
+// add this row to thead
+$('thead').append($headingRow);
+$headingRow.append($('<th/>').html('<a data-sort="firstName">First Name</a>'));
+$headingRow.append($('<th/>').html('<a data-sort="lastName">Last Name</a>'));
+$headingRow.append($('<th/>').html('<a data-sort="gender">Gender</a>'));
+$headingRow.append($('<th/>').html('<a data-sort="role">Role</a>'));
+$headingRow.append($('<th/>').html('<a data-sort="age">Age</a>'));
+$headingRow.append($('<th/>').html('<a data-sort="dateOfBirth">Date Of Birth</a>'));
+
+// get content of json file 
+$.ajax({
+    type: "get",
+    url: "characters.json",
+    error: function () {
+        $('.sortable').empty().append('<h1> Content can not be retrieved</h1>');
+    },
+    success: function (response) {
+        // loop through response received
+        $.each(response, function (index, value) {
+            // create row
+            let $row = $('<tr/>').addClass('row');
+            // add td to the row
+            $row.append($('<td></td>').text(value.firstName));
+            $row.append($('<td></td>').text(value.lastName));
+            $row.append($('<td></td>').text(value.gender));
+            $row.append($('<td></td>').text(value.role));
+            $row.append($('<td></td>').text(value.age));
+            $row.append($('<td></td>').text(value.dateOfBirth));
+
+            // add rows to table 
+            $('tbody').append($row);
+        });
+
+        // Start Sorting
+        var compare = {
+            name: function (a, b) {
+                a = a.replace(/^the /i, '');
+                b = b.replace(/^the /i, '');
+
+                if (a < b) {
+                    return -1;
+                } else {
+                    return a > b ? 1 : 0;
+                }
+            },
+            age: function (a, b) {
+                return a - b;
+            },
+            date: function (a, b) {
+                a = new Date(a);
+                b = new Date(b);
+
+                return a - b;
             }
-    });
+        };
+
+        $('.sortable').each(function () {
+            var $table = $(this);
+            var $tbody = $table.find('tbody');
+            var $controls = $table.find('th a');
+            var rows = $tbody.find('tr').toArray();
+            // copy/clone the row that is an existing row
+            const deepCopy = [...rows];
+
+            $controls.on('click', function () {
+                var $header = $(this);
+                var order = $header.data('sort');
+                var column;
+
+                // If selected item has ascending or descending class, reverse contents
+                if ($header.is('.ascending')) {
+                    $header.removeClass("ascending no-sort");
+                    $header.addClass("descending");
+                    $tbody.append(rows.reverse());
+                } else if ($header.is('.descending')) {
+                    $header.removeClass('descending ascending ');
+                    $header.addClass('no-sort');
+                    $tbody.append(deepCopy);
+                } else {
+                    $header.addClass('ascending');
+                    $header.removeClass('no-sort');
+                    // Remove asc or desc from all other headers
+                    $header.siblings().removeClass('ascending descending no-sort');
+
+                    if (compare.hasOwnProperty(order)) {
+                        column = $controls.index(this);
+                        rows.sort(function (a, b) {
+                            a = $(a).find('td').eq(column).text();
+                            b = $(b).find('td').eq(column).text();
+                            return compare[order](a, b);
+                        });
+                        $tbody.append(rows);
+                    }
+                }
+            });
+        });
+    }
 });
-        function displayCharacters(characters) {
-            $('#charactersTable tbody').empty();
-                $.each(characters, function(index, character) {
-                $('#charactersTable tbody').append(`
-                    <tr>
-                        <td>${character.firstName}</td>
-                        <td>${character.lastName}</td>
-                        <td>${character.gender}</td>
-                        <td>${character.role}</td>
-                        <td>${character.age}</td>
-                    </tr>
-                `   );
-    });
-
-        $('#charactersTable tbody tr').removeClass('highlight');
-    }
-
-    function highlightRows(searchTerm) {
-        searchTerm = searchTerm.toLowerCase();
-        $('#charactersTable tbody tr').each(function() {
-            const firstName = $(this).find('td:first').text().toLowerCase();
-            if (firstName.includes(searchTerm)) {
-                $(this).addClass('highlight');
-            } else {
-                $(this).removeClass('highlight');
-            }
-        });
-}
-    
-function filterCharacters(startLetterA, startLetterB) {
-        const filteredData = charactersData.filter(function(character) {
-            const lastName = character.lastName.charAt(0).toUpperCase();
-            return lastName >= startLetterA && lastName <= startLetterB;
-        });
-
-        displayCharacters(filteredData);
-    }
